0ffd3d63f75566234722cec38174edf6bb049c5c
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-01-05 13:47:40 -0800

    Imports Date component to [id].js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,7 @@
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
+import Date from "../../components/date";
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
@@ -27,11 +28,8 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      {postData.title}
       <br />
-      {postData.id}
-      <br />
-      {postData.date}
+      <Date dateString={postData.date} />
       <br />
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
